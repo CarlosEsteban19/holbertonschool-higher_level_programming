ea564ff3131368a2c08fac1610e6c229c0379396
Charlie <carlos.esteban19@outlook.com>
2024-05-09 14:05:21 -0400

    result

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
-from add_0 import add
 if __name__ == "__main__":
+    result = 0
     if len(argv) == 1:
-        print("0")
+        print(result)
     else:
         for i in range(1, len(argv)):
-            result = add(int(argv[i]), int(argv[i + 1]))
+            result += int(argv[i])
         print(result)
