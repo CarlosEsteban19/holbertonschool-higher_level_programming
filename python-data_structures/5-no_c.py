cc876135e1238782063494418dd2bb57e7d65fce
Charlie <carlos.esteban19@outlook.com>
2024-05-13 12:57:10 -0400

    no c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)):
-        if i == "c" or i == "C":
-            i = ""
-    return my_string
+    new = ""
+    for char in my_string:
+        if char != "c" and char != "C":
+            new += char
+    return new
