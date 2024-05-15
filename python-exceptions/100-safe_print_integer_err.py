7b01ab150b0427d4d21b63b10cfc91f92afd5d4f
Charlie <carlos.esteban19@outlook.com>
2024-05-15 16:01:59 -0400

    stderr

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError, AttributeError) as e:
-        print("Exception:", e)
+    except (ValueError, TypeError) as e:
+        print("Exception:", e, file=sys.stderr)
         return False
