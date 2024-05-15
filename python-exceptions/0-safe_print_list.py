590c22e609560cf77018f893da9103ee9c965646
Charlie <carlos.esteban19@outlook.com>
2024-05-15 14:57:38 -0400

    change print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,8 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end="" if i + 1 < x else "\n")
+            print("{}".format(my_list[i]), end="")
     except IndexError:
         print()
         return i
+    print()
     return i + 1
