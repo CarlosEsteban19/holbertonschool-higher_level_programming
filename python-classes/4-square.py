b10036b1bdd5286eafdb2e5b0223780642f8790c
Charlie <carlos.esteban19@outlook.com>
2024-05-16 14:23:55 -0400

    getter setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class Square:
+    """empty class"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """area of square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """get size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
