b70409b5372e292f578ccf40b303a2a9b9757280
Charlie <carlos.esteban19@outlook.com>
2024-05-16 13:43:27 -0400

    raise

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,9 +5,8 @@
 class Square:
     """empty class"""
     def __init__(self, size=0):
-        try:
-            self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")           
+        self.__size = size
