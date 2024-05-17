7d49ecc4d00f9d2f68b731e6217a82c199fd67fd
Charlie <carlos.esteban19@outlook.com>
2024-05-17 15:29:24 -0400

    is it the same?

---
+++ python-classes/101-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size=0, position=(0, 0)):
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        if type(position) is not tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    @property
+    def size(self):
+        """get size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """get position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """set position"""
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """area of square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints square"""
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
