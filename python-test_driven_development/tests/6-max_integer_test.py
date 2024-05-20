0709e14c7d041fa6cdb8f49315f876a84ff047e4
Charlie <carlos.esteban19@outlook.com>
2024-05-20 16:35:22 -0400

    cut down

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,15 +8,12 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_positive_numbers(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([10, 20, 30, 40]), 40)
 
     def test_negative_numbers(self):
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-        self.assertEqual(max_integer([-10, -20, -30, -40]), -10)
 
     def test_mixed_numbers(self):
         self.assertEqual(max_integer([1, -2, 3, -4]), 3)
-        self.assertEqual(max_integer([-10, 20, -30, 40]), 40)
 
     def test_empty_list(self):
         self.assertIsNone(max_integer([]))
@@ -29,7 +26,6 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_float_numbers(self):
         self.assertEqual(max_integer([1.5, 2.7, 3.2]), 3.2)
-        self.assertEqual(max_integer([-1.5, -2.7, -3.2]), -1.5)
 
     def test_strings(self):
         self.assertEqual(max_integer(["one", "two", "three"]), "four")
