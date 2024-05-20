0318737d91a27836397196c1a9b30446c92f977e
Charlie <carlos.esteban19@outlook.com>
2024-05-20 15:18:00 -0400

    matrix

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""un comentario"""
+
+
+def matrix_divided(matrix, div):
+    """un comentario"""
+    e = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(div) not in (int, float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError(e)
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in row:
+            if type(i) not in (int, float):
+                raise TypeError(e)
+    return list(map(lambda row: list(map(
+        lambda x: round(x / div, 2), row)), matrix))
