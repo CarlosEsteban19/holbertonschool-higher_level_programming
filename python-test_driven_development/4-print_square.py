9395ab19cccf0fed4db0d44020851378448f1f4d
Charlie <carlos.esteban19@outlook.com>
2024-05-20 16:00:45 -0400

    fixes

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,6 +4,9 @@
 
 def print_square(size):
     """comentario"""
+    e = "unsupported operand type(s) for +: 'NoneType' and 'int'"
+    if size is None:
+        raise TypeError(e)
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
