c3b27fd3c4179f387d4489784e65b51001590ade
Charlie <carlos.esteban19@outlook.com>
2024-05-20 16:39:10 -0400

    ok

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""comentario
+"""Unittest for max_integer([..])
 """
 import unittest
 max_integer = __import__("6-max_integer").max_integer
@@ -7,14 +7,11 @@ max_integer = __import__("6-max_integer").max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def test_positive_numbers(self):
+    def test_regular_list(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def test_negative_numbers(self):
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-
-    def test_mixed_numbers(self):
-        self.assertEqual(max_integer([1, -2, 3, -4]), 3)
+    def test_unordered_list(self):
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
     def test_empty_list(self):
         self.assertIsNone(max_integer([]))
@@ -22,15 +19,15 @@ class TestMaxInteger(unittest.TestCase):
     def test_single_element_list(self):
         self.assertEqual(max_integer([5]), 5)
 
-    def test_duplicate_elements(self):
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -5, -3, -2]), -1)
+
+    def test_duplicate_max(self):
         self.assertEqual(max_integer([5, 5, 5, 5]), 5)
 
-    def test_float_numbers(self):
-        self.assertEqual(max_integer([1.5, 2.7, 3.2]), 3.2)
-
-    def test_strings(self):
-        self.assertEqual(max_integer(["one", "two", "three"]), "four")
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([1, -2, 10, 0]), 10)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
