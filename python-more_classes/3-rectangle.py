cba178b1b8e3833aa39d1ce5fb060f8a47a644d9
Charlie <carlos.esteban19@outlook.com>
2024-05-21 13:20:56 -0400

    __str__

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -66,6 +66,3 @@ class Rectangle:
             if i != self.__height - 1:
                 rectangle += "\n"
         return rectangle
-
-    def __repr__(self):
-        return "Rectangle({}, {})".format(self.__width, self.__height)
