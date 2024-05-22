f2d20aea7b889f6dcc0e223f1ca6df612ea229be
Charlie <carlos.esteban19@outlook.com>
2024-05-22 16:35:54 -0400

    swaapped order

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -10,8 +10,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """class derived from Rectangle"""
     def __init__(self, size):
-        super().__init__(size, size)
         super().integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
