b42ee02adf1ca21341d935e629f5dc02e08e20fc
Charlie <carlos.esteban19@outlook.com>
2024-05-22 16:34:03 -0400

    del extra import

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,14 +4,12 @@ almost empty class BaseGeometry
 """
 
 
-BaseGeomerty = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """class derived from Rectangle"""
     def __init__(self, size):
-
         super().__init__(size, size)
         super().integer_validator("size", size)
         self.__size = size
