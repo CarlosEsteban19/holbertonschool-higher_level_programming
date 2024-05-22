7d0fe3e44e06d43fa578372b95060bdfbf68e216
Charlie <carlos.esteban19@outlook.com>
2024-05-22 14:59:53 -0400

    int validation

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+almost empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """class definition"""
+    def area(self):
+        """returns area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if value is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
