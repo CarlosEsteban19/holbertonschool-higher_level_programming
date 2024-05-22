b3af500f436f543d7f6d360f14bfbb5dec0af4c2
Charlie <carlos.esteban19@outlook.com>
2024-05-22 16:25:50 -0400

    square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+almost empty class BaseGeometry
+"""
+
+
+BaseGeomerty = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeomerty):
+    """class derived from base geometry"""
+    def __init__(self, width, height):
+
+        # deal with width
+        super().integer_validator("width", width)
+        self.__width = width
+
+        # deal with height
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """return area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """class derived from Rectangle"""
+    def __init__(self, size):
+
+        super().__init__(size, size)
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area of square"""
+        return self.__size ** 2
