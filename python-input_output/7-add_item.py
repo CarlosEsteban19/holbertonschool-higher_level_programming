64965b2c2887cdb76d7181b8ab48fad51cfaab8d
Charlie <carlos.esteban19@outlook.com>
2024-05-28 15:09:21 -0400

    changed import

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """python I/O task 7"""
-from sys import argv
-"""adds all arguments to a Python list, and then save them to a file"""
+import sys
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__(
@@ -11,5 +11,5 @@ try:
     thelist = load_from_json_file("add_item.json")
 except FileNotFoundError:
     thelist = []
-thelist.extend(argv[1:])
+thelist.extend(sys.argv[1:])
 save_to_json_file(thelist, "add_item.json")
