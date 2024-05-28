691d3844725aed17703a7bbb172604d0818ad5c7
Charlie <carlos.esteban19@outlook.com>
2024-05-28 13:54:03 -0400

    fixed load

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 def load_from_json_file(filename):
     """creates an Object from a “JSON file”"""
     with open(filename, 'r', encoding="utf-8") as f:
-        json.load(filename, f)
+        json.load(f)
         f.close()
