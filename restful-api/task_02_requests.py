bc0b9c416e511c92bdfcc1079cc51a8d401eeb33
Charlie <carlos.esteban19@outlook.com>
2024-05-30 16:33:42 -0400

    handle data from api

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""restful api task 2"""
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    """fetches all post from JSONPlaceholder"""
+    data = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f"Status Code: {data.status_code}")
+    if data.status_code == 200:
+        for post in data.json():
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+    """fetches all post from JSONPlaceholder"""
+    data = requests.get('https://jsonplaceholder.typicode.com/posts')
+    if data.status_code == 200:
+        postlist = []  # emptylist to append dictionaries
+        for post in data.json():
+            postlist.append({'id': post['id'], 'title': post['title'],
+                             'body': post['body']})
+
+    with open("posts.csv", 'w', encoding="utf-8") as file:
+        map = ['id', 'title', 'body']  # define column headers
+        author = csv.DictWriter(file, fieldnames=map)
+
+        author.writeheader()
+        for post in postlist:
+            author.writerow(post)
