cdb97a25e5f86ead3e88f02b8666ed6378618369
Charlie <carlos.esteban19@outlook.com>
2024-05-31 14:45:42 -0400

    status code int

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -32,7 +32,7 @@ class HttpHandler(http.server.BaseHTTPRequestHandler):
                     "A simple API built with http.server"}
             self.wfile.write(json.dumps(info).encode("utf-8"))
         else:
-            self.send_response("404 not found")
+            self.send_response(404)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(b"404 not found")
