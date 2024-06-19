db2a40abd6ddd57f7adb64fc5851b87b7fde0144
Charlie <carlos.esteban19@outlook.com>
2024-06-18 21:45:55 -0400

    just id, name

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,6 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT *
-FROM hbtn_0d_usa.cities
+SELECT id,
+    name
+FROM cities
 WHERE state_id = states.name("California")
 ORDER BY cities.id ASC;
