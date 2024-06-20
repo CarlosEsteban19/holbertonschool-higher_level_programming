b4d4192fd7406b76407f9a18cf09538ee3715470
Charlie <carlos.esteban19@outlook.com>
2024-06-20 16:12:05 -0400

    added binary

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,24 +7,19 @@ import sys
 def list_states(username, password, database_name):
     """List states with letter N"""
 
-    try:
-        conn = MySQLdb.connect(host='localhost',
-                               port=3306,
-                               user=username,
-                               password=password,
-                               db=database_name)
-        cursor = conn.cursor()
-        cursor.execute(
-            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-
-        cursor.close()
-        conn.close()
-
-    except MySQLdb.Error as e:
-        print(f"Error connectig to MySQL or execuiting query: {e}")
+    conn = MySQLdb.connect(host='localhost',
+                           port=3306,
+                           user=username,
+                           password=password,
+                           db=database_name)
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    conn.close()
 
 
 if __name__ == "__main__":
