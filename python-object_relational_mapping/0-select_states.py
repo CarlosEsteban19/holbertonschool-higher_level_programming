fbdbd852cec0b04470f2bbae3b0095fcd2921ea5
Charlie <carlos.esteban19@outlook.com>
2024-06-20 15:10:03 -0400

    try excpt

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,23 +6,27 @@ import MySQLdb
 
 def list_states(username, password, db_name):
     """list all states from database table"""
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )  # Connection
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name
+        )  # Connection
 
-    cursor = db.cursor()  # Object that interacts with database
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+        cursor = db.cursor()  # Object that interacts with database
+        cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
 
-    cursor.close()
-    db.close()
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print(e)
 
 
 if __name__ == "__main__":
