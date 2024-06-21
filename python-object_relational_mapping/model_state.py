727382722e03cccee07475157dba951a927d652d
Charlie <carlos.esteban19@outlook.com>
2024-06-21 13:54:58 -0400

    task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""task 6"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class defining starte"""
+    __tablename__ = "states"
+    id = Column("id", Integer, autoincrement=True, primary_key=True)
+    name = Column("name", String(128), nullable=False)
