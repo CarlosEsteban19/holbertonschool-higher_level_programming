094fbe337f4f8fd9746dde291ce839f2b5fe292a
Charlie <carlos.esteban19@outlook.com>
2024-06-21 16:14:22 -0400

    task 14

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""task 14"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """class defining starte"""
+    __tablename__ = "cities"
+    id = Column("id", Integer, autoincrement=True, primary_key=True)
+    name = Column("name", String(128), nullable=False)
+    state_id = Column(
+        "state_id", Integer, ForeignKey("states.id"), nullable=False)
