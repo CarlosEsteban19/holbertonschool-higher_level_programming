13e22d8d34edc376dddfaa487554caaa48efab7d
Charlie <carlos.esteban19@outlook.com>
2024-07-28 22:49:24 -0400

    task 6

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,10 @@
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+const character = document.getElementById('character');
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        character.textContent = data.name;
+    })
+    .catch((error) => {
+        console.error("Error: ", error);
+    });
